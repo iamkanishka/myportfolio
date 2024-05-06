3efcee415d3677b90066030fe0cec6cefec8d5f8
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2024-05-06 15:03:51 +0530

    Added Find by Id API for Project Deatils

--- src/app/firebase-db/MongodbRESTAPIDB/rest-apiservice.service.ts
+++ src/app/firebase-db/MongodbRESTAPIDB/rest-apiservice.service.ts
@@ -13,54 +13,67 @@ export class RestAPIServiceService {
     projectorArticledata: ProjectorArticle
   ): Promise<any> {
     try {
-      return await this.http.post(`${environment.MongoRESTAPIURL}${dataType}`, projectorArticledata).toPromise()
+      return await this.http
+        .post(`${environment.MongoRESTAPIURL}${dataType}`, projectorArticledata)
+        .toPromise();
     } catch (err) {
       console.log(err);
-      
     }
   }
 
-async  updateDoc(
-  dataType: string,
-  projectorArticledata: ProjectorArticle,
-  projectOrArticleId: string
-): Promise<any> {
-  try {
-    return await this.http.patch(`${environment.MongoRESTAPIURL}${dataType}/${projectOrArticleId}`, projectorArticledata).toPromise()
-  } catch (err) {
-    return 
-    console.log(err);
-    
+  async updateDoc(
+    dataType: string,
+    projectorArticledata: ProjectorArticle,
+    projectOrArticleId: string
+  ): Promise<any> {
+    try {
+      return await this.http
+        .patch(
+          `${environment.MongoRESTAPIURL}${dataType}/${projectOrArticleId}`,
+          projectorArticledata
+        )
+        .toPromise();
+    } catch (err) {
+      return;
+      console.log(err);
+    }
   }
-}
 
-async  deleteDoc(
-  dataType: string, projectOrArticleId: string
-): Promise<any> {
-  try {
-    return await this.http.delete(`${environment.MongoRESTAPIURL}${dataType}/${projectOrArticleId}`,).toPromise()
-  } catch (err) {
-    return 
-    console.log(err);
-    
+  async deleteDoc(dataType: string, projectOrArticleId: string): Promise<any> {
+    try {
+      return await this.http
+        .delete(
+          `${environment.MongoRESTAPIURL}${dataType}/${projectOrArticleId}`
+        )
+        .toPromise();
+    } catch (err) {
+      return;
+      console.log(err);
+    }
   }
-}
 
-
-async  GetDocsBySearch(
-  dataType: string, query: string): Promise<any> {
-  try {
-    return await this.http.get(`${environment.MongoRESTAPIURL}${dataType}/search/${query}`,).toPromise()
-  } catch (err) {
-    return 
-    console.log(err);
-    
+  async GetDocsBySearch(dataType: string, query: string): Promise<any> {
+    try {
+      return await this.http
+        .get(`${environment.MongoRESTAPIURL}${dataType}/search/${query}`)
+        .toPromise();
+    } catch (err) {
+      return;
+      console.log(err);
+    }
   }
-}
 
 
-
-
-
+  
+  async GetDocbyUniqueId(dataType: string, query: string): Promise<any> {
+    try {
+      return await this.http
+        .get(`${environment.MongoRESTAPIURL}${dataType}/findByUniqueId/${query}`)
+        .toPromise();
+    } catch (err) {
+      return;
+      console.log(err);
+    }
+  }
 
 }
