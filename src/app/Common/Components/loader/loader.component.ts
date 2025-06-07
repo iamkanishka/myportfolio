874bb4d7d72a52174df301bf081bd6d2f20176fa
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-06-07 21:59:59 +0530

    chore: Added standalone property to decorator

--- src/app/Common/Components/loader/loader.component.ts
+++ src/app/Common/Components/loader/loader.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-loader',
+  standalone: true,
+
   template: `
   <div class="flex  flex-wrap justify-center items-center " >
     <img src="./assets/loader.jpeg" alt="" height="200" width="200" />
