73df1a5de69ea841387666dca8d17f6252bbbfa4
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-06-23 21:18:30 +0530

    chore: minir change

--- src/common/components/tags/tags.ts
+++ src/common/components/tags/tags.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
-import { Tag } from '../../utilities/Data';
+import { Tag } from '../../utilities/data';
 
 interface ICustomOption {
   totalTagsVisible: number;
