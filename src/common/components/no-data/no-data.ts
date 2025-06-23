8b746916e742825faa6184c1a8a9f909344e3dfc
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-06-23 21:15:02 +0530

    feat: No-data comp

--- src/common/components/no-data/no-data.ts
+++ src/common/components/no-data/no-data.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-no-data',
   standalone: true,
 
-   template: `
-    <div class="flex  flex-wrap justify-center items-center " >
+  template: `
+    <div class="flex  flex-wrap justify-center items-center ">
       <img src="./assets/notfound.jpeg" alt="" height="200" width="200" />
     </div>
   `,
