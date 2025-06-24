fe77a1e2941451f99e479ce721f54f8eb3958638
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-06-24 19:36:39 +0530

    feat: Added Admin and public lazy routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./public/public.routes').then((m) => m.PublicRoutingModule),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.routes').then((m) => m.AdminRoutingModule),
+  },
+];
