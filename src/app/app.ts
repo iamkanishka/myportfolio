1e34629103720fcf762ffd0db444bb5b15d13d2d
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-06-24 19:37:06 +0530

    chore: Added router outlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { BlackHoleComponent } from '../common/components/black-hole/black-hole';
+
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [BlackHoleComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  protected title = 'myportfolio';
+ 
 }
