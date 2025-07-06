779cc21085cf9378e8566054fd99fea11fbcc52b
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-07-06 21:23:04 +0530

    feat: Auth Interceptor

---
+++ src/interceptors/auth.interceptor.ts
@@ -0,0 +1,31 @@
+// auth.interceptor.ts
+import { inject, Injectable } from '@angular/core';
+import {
+  HttpInterceptorFn,
+  HttpRequest,
+  HttpHandlerFn,
+  HttpEvent,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+
+export const AuthInterceptor: HttpInterceptorFn = (
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn
+): Observable<HttpEvent<unknown>> => {
+  const token = localStorage.getItem('token');
+  const router = inject(Router);
+
+  if (token) {
+    const cloned = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return next(cloned);
+  } else {
+    router.navigate(['/admin/auth']);
+  }
+
+  return throwError(() => new Error('Unauthorized: No token'));
+};
